7e3a75bd5e17eec7b73030f8e4bd547b76e7c0ec
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-02-03 11:04:18 +0330

    Update jettons configuration with predefined addresses
    Revised the config to include predefined addresses for several jettons such as USDT, USDC, NOT, HMSTR, and DOGS. Also corrected the key name typo from 'jettrons' to 'jettons'. This improves clarity and readiness for managing jetton addresses.

--- config/ton-watcher.php
+++ config/ton-watcher.php
@@ -3,7 +3,13 @@
 return [
     'api-key' => '',
     'address' => '',
-    'jettrons' => [
-        'USDT' => '',
+    'jettons' => [
+        'USDT' => 'EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs',
+        'USDC' => 'EQB-MPwrd1G6WKNkLz_VnV6WqBDd142KMQv-g1O-8QUA3728',
+        'NOT' => 'EQAvlWFDxGF2lXm67y4yzC17wYKD9A0guwPkMs1gOsM__NOT',
+        'HMSTR' => 'EQAJ8uWd7EBqsmpSWaRdf_I-8R8-XHwh3gsNKhy-UrdrPcUo',
+        'DOGS' => 'EQCvxJy4eG8hyHBFsZ7eePxrRsUQSFE_jpptRAYBmcG_DOGS',
+
+        // You can add any other Jettons here.
     ]
 ];
