a1e08876c8d46a6358fb79bf40c0b9d6f59252be
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-02-07 10:11:27 +0330

    Add address validation using `Address::isValid` method
    Introduced a new `isValid` method to validate TON addresses. This helps ensure input addresses are correctly formatted before processing. Also added the necessary `Address` import to support this functionality.

--- src/TonWatcher.php
+++ src/TonWatcher.php
@@ -2,6 +2,7 @@
 
 namespace AliQasemzadeh\LaravelTonWatcher;
 
+use Catchain\Ton\Address\Address;
 use Illuminate\Support\Facades\Http;
 
 class TonWatcher
@@ -9,5 +10,11 @@ class TonWatcher
     public static function txs(string $address = null): void|array
     {
         $data = Http::withHeaders(['x-api-key' => config('tonwatcher.x-api-key')])->get('https://tonapi.io/v2/accounts/'.config('tonwatcher.address').'/events?limit='.config('tonwatcher.page-limit'));
+
+    }
+
+    public static function isValid(string $address): bool
+    {
+        return Address::isValid($address);
     }
 }
