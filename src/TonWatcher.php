0bd157070774eb1f5cf720b6e8ea19e5d33ec227
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-02-13 09:28:07 +0330

    Refactor `txs` method to filter and return valid transactions
    The `txs` method now filters out scam events and processes only successful `JettonTransfer` and `TonTransfer` actions, returning structured transaction data. It also supports using a custom watcher address if provided. This improves data handling and ensures only valid transactions are returned.

--- src/TonWatcher.php
+++ src/TonWatcher.php
@@ -7,10 +7,37 @@ use Illuminate\Support\Facades\Http;
 
 class TonWatcher
 {
-    public static function txs(string $address = null): void|array
+    public static function txs(string $address = null): array
     {
-        $data = Http::withHeaders(['x-api-key' => config('tonwatcher.x-api-key')])->get('https://tonapi.io/v2/accounts/'.config('tonwatcher.address').'/events?limit='.config('tonwatcher.page-limit'));
-        return $data;
+        $txs = [];
+        if($address) {
+            $watcherAddress = $address;
+        } else {
+            $watcherAddress = config('tonwatcher.address');
+        }
+        $data = Http::withHeaders(['x-api-key' => config('tonwatcher.x-api-key')])->get('https://tonapi.io/v2/accounts/'.$watcherAddress.'/events?limit='.config('tonwatcher.page-limit'));
+        if ($data->successful()) {
+            $events = $data->json()['events'];
+            foreach ($events as $event) {
+                if (!$event['is_scam']) {
+                    foreach ($event['actions'] as $action) {
+                        if ($action['type'] == 'JettonTransfer') {
+                            if ($action['status'] == 'ok') {
+                                $transaction = $action['JettonTransfer'];
+                                $txs[] = $transaction;
+                            }
+                        }
+                        if ($action['type'] == 'TonTransfer') {
+                            if ($action['status'] == 'ok') {
+                                $transaction = $action['TonTransfer'];
+                                $txs[] = $transaction;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return $txs;
     }
 
     public static function isValid(string $address): bool
